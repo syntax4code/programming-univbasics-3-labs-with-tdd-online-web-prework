d2c19dcffe3791511564e1fb80efdb5bfb7d27ff
Warren Loy Harper II <warrenharper2@att.net>
2019-07-17 22:32:04 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,20 @@
-# Add your variables here
+# Code your solution in calculator.rb
+puts "Enter First Number: "
+first_number = gets.chomp () .to_f
+puts "Enter operator: "
+op = gets.chomp()
+puts "Enter second number: "
+second_number = gets.chomp().to_f
+# add, subtract, multiply or divide
+if op == "+"
+  puts (first_number + second_number)
+  elsif op == "-"
+   puts (first_number - second_number)
+ elsif op == "*"
+   puts (first_number * second_number)
+   elsif op == "/"
+   puts (first_number / second_number)
+   # if invalid character is entered
+ else
+   puts "Invalid Operator"
+ end
